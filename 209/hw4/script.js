7a717bb1b719938eed3db90c5e5b7696267ad659
Sean Underwood <flyinghamsterster@gmail.com>
2016-06-29 22:19:24 -0500

    Perfectionism! Adding new button for color toggling and comments

--- 209/hw4/script.js
+++ 209/hw4/script.js
@@ -1,7 +1,28 @@
+// In my defense: 
+// I realize the way this code works is not terribly idiomatic, and 
+// isn't even all that consistent in the idioms it uses. In some 
+// spots (especially around event handlers) I'm fighting with
+// JavaScript rather than working with it.
+//
+// I've learned quite a lot about JavaScript over the past few weeks, 
+// so suffice it to say that I would organize this diffierently if I 
+// were working from a fresh start. For now, though, I figure the point
+// of this assignment is to build/demonstrate familiarity with D3, so
+// I didn't want to wander off in the weeds by worrying about refactoring. 
+// -su
+
+
+// Array for mapping between day numbers and day names.
+// Ordinals in this array correspond to dayOfWeek for dates.
 var weekDays = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 
 				'Thursday',	'Friday', 'Saturday'];
 
+var c10 = d3.scale.category10();
+
+// Data for days in this set will be displayed in the graph.
 var selectedDays = new Set(weekDays);
+
+var showColors = false;
 
 function initialize(plt) {
 	plt.contentWidth = getContentSize(plt.width, plt.padding);
@@ -10,14 +31,18 @@ function initialize(plt) {
 	d3.csv(plt.datasource, function(d) { buildPlot(plt, d); });
 };
 
+// Helper for generating translate style strings
 function translate(x, y) {
 	return "translate(" + x + "," + y + ")";
 };
 
+// Gets the amount of space for graph content, 
+// equal to total length/width minus padding on each end.
 function getContentSize(size, padding) {
 	return size - (padding * 2);	
 }
 
+// Entry point for drawing everything that D3 is in charge of.
 function buildPlot(plt, data) {	
 	addDayOfWeek(data);
 
@@ -28,6 +53,7 @@ function buildPlot(plt, data) {
 	plt.yAxis = buildAxis(plt.yScale, "left");
 
 	createDayPicker(plt, data);
+	attachColorToggle(plt, data);
 
 	plt.tooltip = createTooltipElement(plt);
 
@@ -35,6 +61,8 @@ function buildPlot(plt, data) {
 	drawGraph(plt, data);
 };
 
+// Augments the elements in the data array with DayOfWeek values
+// so that they don't need to keep getting recalculated.
 function addDayOfWeek(data) {
 	data.forEach(function (d) {
 		date = new Date(d.date);
@@ -79,7 +107,22 @@ function createDayPicker(plt, data) {
 				"checked": "checked"
 			})
 			.on("change", 
-				function() { onDayCheckboxClick(plt, data, this);});
+				function() { onDayCheckboxClick(plt, data, this); });
+
+	updateDayPickerColors(plt);
+}
+
+function updateDayPickerColors(plt) {
+	d3.select(plt.daysForm)
+		.selectAll("label")
+		.data(weekDays)
+		.transition()
+		.style("color", selectColorForDayName)	
+}
+
+function attachColorToggle(plt, data) {
+	d3.select("#colorToggle")
+		.on("click", function() { onColorToggleClick(plt, data); });
 }
 
 function createTooltipElement(plt) {
@@ -147,14 +190,17 @@ function drawData(plt, data) {
 		.on("mouseover", function(d) { onMouseOver(plt, d, this) })
 		.on("mouseout", function(d) { onMouseOut(plt, this) });
 
-	updateDataVisibility(plt, data);
+	updateDataStyling(plt, data);
 }
 
-function updateDataVisibility(plt, data) {
+function updateDataStyling(plt, data) {
 	plt.graph.selectAll("circle")
 		.data(data)
 		.transition()
-		.style("opacity", selectOpacity);
+		.style({
+			"opacity": selectOpacity,
+			"fill": selectColorForDataPoint 
+		});
 }
 
 function selectOpacity(d) {
@@ -169,6 +215,22 @@ function dayIsSelected(d) {
 	return selectedDays.has(d.dayOfWeek);
 }
 
+function selectColorForDataPoint(d) {
+	return selectColorForDayName(d.dayOfWeek);
+}
+
+function selectColorForDayName(day) {
+	return selectColor(weekDays.indexOf(day));
+}
+
+function selectColor(d) {
+	if(showColors) {
+		return c10(d);
+	} else {
+		return "rgb(0,0,0)";
+	}
+}
+
 function onDayCheckboxClick(plt, data, src) {
 	if(src.checked) 
 	{
@@ -179,7 +241,14 @@ function onDayCheckboxClick(plt, data, src) {
 		selectedDays.delete(src.id);
 	}
 
-	updateDataVisibility(plt, data);
+	updateDataStyling(plt, data);
+}
+
+function onColorToggleClick(plt, d) {
+	debugger;
+	showColors = !showColors;
+	updateDayPickerColors(plt);
+	updateDataStyling(plt, d);
 }
 
 function onMouseOver(plt, d, target) {
@@ -202,16 +271,10 @@ function onMouseOver(plt, d, target) {
 		.style("opacity", 1);
 }
 
-
 function onMouseOut(plt, target) {
 	d3.select(target)
 		.transition()
 		.duration(100)
 		.attr("fill", "black");
-
-	// plt.tooltip
-	// 	.transition()
-	// 	.duration(100)
-	// 	.style("opacity", 0);	
 }
 
